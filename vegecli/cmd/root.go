f375810753704d17b17ffffca1bbf7364f148765
hongyu <lhy429581492@gmail.com>
2023-03-13 17:01:12 +0800

    vegecli -t

--- vegecli/cmd/root.go
+++ vegecli/cmd/root.go
@@ -21,6 +21,13 @@ const (
 	Cmd_ipsb     = "ipsb"     //curl ip.sb
 )
 
+var cmdL = []string{
+	Cmd_newps,
+	Cmd_localip,
+	Cmd_netinter,
+	Cmd_ipsb,
+}
+
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "vegecli",
@@ -34,7 +41,13 @@ to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
-
+		if *t {
+			fmt.Println("helping cmd list:")
+			for _, v := range cmdL {
+				fmt.Println(v)
+			}
+			return
+		}
 		if len(args) > 0 {
 			switch args[0] {
 			case Cmd_newps:
@@ -89,6 +102,8 @@ func Execute() {
 	}
 }
 
+var t *bool
+
 func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
@@ -98,5 +113,5 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	t = rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle cmd list")
 }
