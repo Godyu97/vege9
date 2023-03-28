8e0e1e8038d7b0089078301aae6b922d5b5a38b4
hongyu <lhy429581492@gmail.com>
2023-03-28 14:58:06 +0800

    pcre add

--- vegePcre/PcreToRe2.go
+++ vegePcre/PcreToRe2.go
@@ -134,7 +134,7 @@ func toheap(ptr *C.pcre) (re Regexp) {
 
 // Try to compile the pattern.  If an error occurs, the second return
 // value is non-nil.
-func Compile(pattern string, flags int) (Regexp, *CompileError) {
+func Compile(pattern string, flags int) (Regexp, error) {
 	pattern1 := C.CString(pattern)
 	defer C.free(unsafe.Pointer(pattern1))
 	if clen := int(C.strlen(pattern1)); clen != len(pattern) {
@@ -144,6 +144,7 @@ func Compile(pattern string, flags int) (Regexp, *CompileError) {
 			Offset:  clen,
 		}
 	}
+
 	var errptr *C.char
 	var erroffset C.int
 	ptr := C.pcre_compile(pattern1, C.int(flags), &errptr, &erroffset, nil)
@@ -404,6 +405,6 @@ type CompileError struct {
 	Offset  int
 }
 
-func (e *CompileError) String() string {
+func (e *CompileError) Error() string {
 	return e.Pattern + " (" + strconv.Itoa(e.Offset) + "): " + e.Message
 }
