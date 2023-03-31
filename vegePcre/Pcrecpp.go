b9613b07c6d851f81a38b954f288adcc62c3ad0b
hongyu <lhy429581492@gmail.com>
2023-03-31 09:38:07 +0800

    test cgo ++

--- vegePcre/Pcrecpp.go
+++ vegePcre/Pcrecpp.go
@@ -7,17 +7,13 @@ package vegePcre
 */
 import "C"
 
-import (
-	"unsafe"
-)
-
 func Replace(pattern string, repl string, src string) string {
 	pattern1 := C.CString(pattern)
-	defer C.free(unsafe.Pointer(pattern1))
+	//defer C.free(unsafe.Pointer(pattern1))
 	src1 := C.CString(src)
-	defer C.free(unsafe.Pointer(src1))
+	//defer C.free(unsafe.Pointer(src1))
 	repl1 := C.CString(repl)
-	defer C.free(unsafe.Pointer(repl1))
+	//defer C.free(unsafe.Pointer(repl1))
 	C.Pcrepp_Replace(pattern1, repl1, src1)
 	return string(src1)
 }
