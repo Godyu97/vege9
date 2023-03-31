9cde827b7cb406637611cfa51b27e4edeecbfdd9
hongyu <lhy429581492@gmail.com>
2023-03-31 09:57:31 +0800

    test cgo ++

--- vegePcre/mypcre.cpp
+++ vegePcre/mypcre.cpp
@@ -1,15 +1,16 @@
+#ifndef MYPCRE_H
+#define MYPCRE_H
 
 #include <pcre++.h>
+
 #include <cstring>
 #include <iostream>
 extern "C"{
-
 void Pcrepp_Replace(char* patten, char* repl, char* src) {
-  //   std::string str = "Hello (world)!";
-  //   std::string replace_str = "<$1>";
-  //   std::string patten = "\\((.*?)\\)";
   pcrepp::Pcre re(patten);
   std::string res = re.replace(std::string(src), std::string(repl));
   std::strcpy(src, res.c_str());
 }
 }
+
+#endif
