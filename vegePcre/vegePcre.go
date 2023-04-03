4cfdc8df3924139005455607ce7fcd6493dffb48
hongyu <lhy429581492@gmail.com>
2023-04-03 11:07:39 +0800

    test cgo ++

--- vegePcre/vegePcre.go
+++ vegePcre/vegePcre.go
@@ -13,6 +13,9 @@
 package vegePcre
 
 /*
+#cgo LDFLAGS: -lpcre++
+#cgo CFLAGS: -std=c++11 -I/usr/include
+
 #define intgo swig_intgo
 typedef void *swig_voidp;
 
@@ -43,28 +46,26 @@ import "unsafe"
 import _ "runtime/cgo"
 import "sync"
 
-
 type _ unsafe.Pointer
-
-
 
 var Swig_escape_always_false bool
 var Swig_escape_val interface{}
 
-
 type _swig_fnptr *byte
 type _swig_memberptr *byte
 
-
 type _ sync.Mutex
 
+type swig_gostring struct {
+	p uintptr
+	n int
+}
 
-type swig_gostring struct { p uintptr; n int }
 func swigCopyString(s string) string {
-  p := *(*swig_gostring)(unsafe.Pointer(&s))
-  r := string((*[0x7fffffff]byte)(unsafe.Pointer(p.p))[:p.n])
-  Swig_free(p.p)
-  return r
+	p := *(*swig_gostring)(unsafe.Pointer(&s))
+	r := string((*[0x7fffffff]byte)(unsafe.Pointer(p.p))[:p.n])
+	Swig_free(p.p)
+	return r
 }
 
 func Swig_free(arg1 uintptr) {
@@ -96,8 +97,6 @@ func Pcrepp_Replace(arg1 string, arg2 string, arg3 string) (_swig_ret string) {
 		Swig_escape_val = arg3
 	}
 	var swig_r_1 string
- swig_r_1 = swigCopyString(swig_r) 
+	swig_r_1 = swigCopyString(swig_r)
 	return swig_r_1
 }
-
-
