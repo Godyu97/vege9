237497381de2513e1942b8be977c9d14c72bf4ae
hongyu <lhy429581492@gmail.com>
2023-05-19 16:15:38 +0800

    worker pool impl

---
+++ vegeTools/workpool_test.go
@@ -0,0 +1,32 @@
+package vegeTools
+
+import (
+	"testing"
+	"time"
+)
+
+func TestWorkerPool_Submit(t *testing.T) {
+	tasks := NewWorkerPool(128)
+	for i := 0; i < 1000; i++ {
+		ii := i
+		tasks.Submit(func() {
+			time.Sleep(time.Second)
+			t.Log(ii, " done")
+		})
+	}
+	t.Log("tasks StopWait log")
+	tasks.StopWait()
+}
+
+func TestNewWorkerPool_SubmitBlockBySize(t *testing.T) {
+	tasks := NewWorkerPool(128)
+	for i := 0; i < 1000; i++ {
+		ii := i
+		tasks.SubmitBlockBySize(func() {
+			time.Sleep(time.Second)
+			t.Log(ii, " done")
+		})
+	}
+	t.Log("tasks StopWait log")
+	tasks.StopWait()
+}
