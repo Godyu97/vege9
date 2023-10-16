c007ddfeca437a07d65c1b27df0a1b7ad27d9f04
hongyu <lhy429581492@gmail.com>
2023-10-16 10:30:56 +0800

    feat:log level

---
+++ vegeLog/level.go
@@ -0,0 +1,14 @@
+package vegeLog
+
+import (
+	"go.uber.org/zap/zapcore"
+)
+
+// debug mode：Info模式false，Debug模式true
+func JudgeLogLevel(debug bool) zapcore.Level {
+	if debug == false {
+		return zapcore.InfoLevel
+	} else {
+		return zapcore.DebugLevel
+	}
+}
