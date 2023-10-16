bb6798f8920f4fc67ca0e241b9c97945a9ffb8bf
hongyu <lhy429581492@gmail.com>
2023-10-16 15:14:38 +0800

    feat:SliceGroupsOf

--- vegeTools/slices.go
+++ vegeTools/slices.go
@@ -74,3 +74,32 @@ func SliceEqual[T comparable](s1, s2 []T) bool {
 	}
 	return true
 }
+
+func SliceGroupsOf[T any](arr []T, num int64) [][]T {
+	max := int64(len(arr))
+	//判断数组大小是否小于等于指定分割大小的值，是则把原数组放入二维数组返回
+	if max <= num {
+		return [][]T{arr}
+	}
+	//获取应该数组分割为多少份
+	var quantity int64
+	if max%num == 0 {
+		quantity = max / num
+	} else {
+		quantity = (max / num) + 1
+	}
+	//声明分割好的二维数组
+	var segments = make([][]T, 0)
+	//声明分割数组的截止下标
+	var start, end, i int64
+	for i = 1; i <= quantity; i++ {
+		end = i * num
+		if i != quantity {
+			segments = append(segments, arr[start:end])
+		} else {
+			segments = append(segments, arr[start:])
+		}
+		start = i * num
+	}
+	return segments
+}
