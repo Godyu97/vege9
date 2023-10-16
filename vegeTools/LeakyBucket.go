5c83ff2b8ee2f3ccce469a1bc071809be95ca9b0
hongyu <lhy429581492@gmail.com>
2023-10-16 16:01:08 +0800

    docs:ratelimit

--- vegeTools/LeakyBucket.go
+++ vegeTools/LeakyBucket.go
@@ -5,14 +5,17 @@ import (
 )
 
 // Rate 基于	"go.uber.org/ratelimit" 的漏桶算法限流器的封装
+// 固定速率执行业务函数
 type Rate struct {
 	ratelimit.Limiter
 }
 
+//创建QPS为n的限流器
 func NewRateLeakyBucket(n int64) *Rate {
 	return &Rate{Limiter: ratelimit.New(int(n))}
 }
 
+//定速执行业务函数
 func (r *Rate) RateLimitDo(fn func()) {
 	r.Take()
 	fn()
