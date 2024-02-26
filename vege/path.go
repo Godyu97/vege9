498d2e47d8b2a46a5c921527f1eca090de74eb27
hongyu <lhy429581492@gmail.com>
2024-02-26 16:42:22 +0800

    feat:PathExists

---
+++ vege/path.go
@@ -0,0 +1,17 @@
+package vege
+
+import (
+	"os"
+)
+
+// 判断 file 、dir 是否存在
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
