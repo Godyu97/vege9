61aa45279f18e13bf19a1e313fd7ce73808f1ca8
hongyu <lhy429581492@gmail.com>
2024-08-21 15:44:11 +0800

    feat:logger for gorm

--- test/main.go
+++ test/main.go
@@ -1,31 +1,33 @@
 package main
 
 import (
-	"log"
-	"fmt"
+	"github.com/Godyu97/vege9/vegelog"
 )
 
 func main() {
-	const s = `
-	%s
-  	昨夜雨疏风骤，
-  	浓睡不消残酒。
-	试问卷帘人，
-	却道海棠依旧。
-	知否？知否？
-	应是绿肥红瘦。
-    %s
-`
-	log.Println(fmt.Sprintf(s, "```", "```"))
-	log.Println(fmt.Sprintf(`
-	%s
-  	昨夜雨疏风骤，
-  	浓睡不消残酒。
-	试问卷帘人，
-	却道海棠依旧。
-	知否？知否？
-	应是绿肥红瘦。
-    %s
-`, "```", "```"))
+	//	const s = `
+	//	%s
+	//  	昨夜雨疏风骤，
+	//  	浓睡不消残酒。
+	//	试问卷帘人，
+	//	却道海棠依旧。
+	//	知否？知否？
+	//	应是绿肥红瘦。
+	//    %s
+	//`
+	//	log.Println(fmt.Sprintf(s, "```", "```"))
+	//	log.Println(fmt.Sprintf(`
+	//	%s
+	//  	昨夜雨疏风骤，
+	//  	浓睡不消残酒。
+	//	试问卷帘人，
+	//	却道海棠依旧。
+	//	知否？知否？
+	//	应是绿肥红瘦。
+	//    %s
+	//`, "```", "```"))
+
+	vegelog.InitLogger("./t.log", vegelog.JudgeLogLevel(true))
+	vegelog.SingleVegeLogger().Printf("[warn] %s", "你好")
 
 }
